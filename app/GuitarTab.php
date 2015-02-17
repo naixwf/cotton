179b43262e36639a933f642658ff7878d2619738
naixwf <naixwf@gmail.com>
2015-02-17 10:06:37 +0800

    php artisan make:model GuitarTab

---
+++ app/GuitarTab.php
@@ -0,0 +1,11 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GuitarTab extends Model {
+
+    protected $table = 'guitar_tabs';
+
+    protected $timestamps = false;
+
+}
