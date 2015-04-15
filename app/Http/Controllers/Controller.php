595eb637ea1e5af8e88314a7cacfcb6f2479d1e1
naixwf <naixwf@gmail.com>
2015-04-15 17:55:34 +0800

    调整sql输入开关位置

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,36 +6,35 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Log;
 use Illuminate\Support\Facades\DB;
 
-abstract class Controller extends BaseController {
+abstract class Controller extends BaseController
+{
 
-	use DispatchesCommands, ValidatesRequests;
+    use DispatchesCommands, ValidatesRequests;
 
-	public function __construct()
-	{
-		$this->beforeFilter('@enableQueryLogging');
-		$this->afterFilter('@outputQueryLogging');
-	}
+    public function __construct()
+    {
+        if (env("APP_DEBUG")) {
+            $this->beforeFilter('@enableQueryLogging');
+            $this->afterFilter('@outputQueryLogging');
+        }
+    }
 
-	public function enableQueryLogging($route, $request)
-	{
-		Log::info('Started ' . $request->method() . ' ' . $request->server('PATH_INFO'));
-		if(env("APP_DEBUG")){
-			DB::connection()->enableQueryLog();
-		}
+    public function enableQueryLogging($route, $request)
+    {
+        Log::info('Started ' . $request->method() . ' ' . $request->server('PATH_INFO'));
+        DB::connection()->enableQueryLog();
+    }
 
-	}
-
-	public function outputQueryLogging()
-	{
-		foreach (DB::getQueryLog() as $query)
-		{
-			$replacements = $query['bindings'];
-			$output = preg_replace_callback('/\?/', function($matches) use (&$replacements) {
-				return array_shift($replacements);
-			}, $query['query']);
-			Log::info('(' . $query['time'] . ') ' . $output);
-		}
-	}
+    public function outputQueryLogging()
+    {
+        foreach (DB::getQueryLog() as $query) {
+            $replacements = $query['bindings'];
+            $output = preg_replace_callback('/\?/', function ($matches) use (&$replacements) {
+                return array_shift($replacements);
+            }, $query['query']);
+            Log::info('(' . $query['time'] . ') ' . $output);
+        }
+    }
 
 
 }
